cb6af8b1486ee7ba8f92f97736fb179e6263b313
Alessandro Candon <alessandro.candon@oxylane.com>
2017-11-15 14:09:46 +0100

    refactor test on task

--- tests/AppBundle/Controller/TaskControllerTest.php
+++ tests/AppBundle/Controller/TaskControllerTest.php
@@ -29,17 +29,14 @@ class TaskControllerTest extends CustomWebTestCase
     public function test_get_all_task_controller_database_not_exist_use_default_db()
     {
         $client = self::createClient();
-
         $client->request(
             'GET',
             "/database/databasenotexist"
         );
-
         $client->request(
             'GET',
             "/tasks"
         );
-
         $json = '[{"id":1,"name":"taskname 0","description":"taskdescription0"},{"id":2,"name":"taskname 1","description":"taskdescription1"}]';
 
         $this->assertEquals($json, $client->getResponse()->getContent());
